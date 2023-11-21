0499afdb6e99795b7f2336aaf96bf22b65d61925
Timofey <timafolf2003@mail.ru>
2023-11-21 22:12:29 +0300

    add determinant method in Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,11 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] matrix = fillRandomMatrix(5, 5);
+        int[][] matrix = fillRandomMatrix(3, 3);
         printMatrix(matrix);
+        System.out.println();
+        int determinant = determinant(matrix);
+        System.out.println(determinant);
     }
     public static int[][] fillRandomMatrix(int rows, int cols) {
         Random random = new Random();
@@ -24,5 +27,39 @@ public class Main {
             }
             System.out.println();
         }
+    }
+
+    public static int determinant(int[][] matrix) {
+        int n = matrix.length;
+        if (n == 1) {
+            return matrix[0][0];
+        } else if (n == 2) {
+            return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+        } else {
+            int det = 0;
+            for (int j = 0; j < n; j++) {
+                det += Math.pow(-1, j) * matrix[0][j] * determinant(getSubMatrix(matrix, 0, j));
+            }
+            return det;
+        }
+    }
+
+    // Вспомогательный метод для получения минора матрицы
+    public static int[][] getSubMatrix(int[][] matrix, int excluding_row, int excluding_col) {
+        int n = matrix.length;
+        int[][] subMatrix = new int[n - 1][n - 1];
+        int r = -1;
+        for (int i = 0; i < n; i++) {
+            if (i == excluding_row)
+                continue;
+            r++;
+            int c = -1;
+            for (int j = 0; j < n; j++) {
+                if (j == excluding_col)
+                    continue;
+                subMatrix[r][++c] = matrix[i][j];
+            }
+        }
+        return subMatrix;
     }
 }
