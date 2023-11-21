450e34a5163cd99e68c3e6faa5651f42e60750bc
Timofey <timafolf2003@mail.ru>
2023-11-21 22:09:14 +0300

    add fillRandomMatrix method in Main.java

--- Main.java
+++ Main.java
@@ -1,2 +1,17 @@
+import java.util.Random;
+
 public class Main {
+    public static void main(String[] args) {
+        int[][] matrix = fillRandomMatrix(5, 5);
+    }
+    public static int[][] fillRandomMatrix(int rows, int cols) {
+        Random random = new Random();
+        int[][] matrix = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = random.nextInt(1, 10); // Генерация случайных чисел от 0 до 10 (для примера)
+            }
+        }
+        return matrix;
+    }
 }
