a753bb2833bce0f5334d71b5285b40ec6dcfc293
Timofey <timafolf2003@mail.ru>
2023-11-21 21:39:20 +0300

    add getUnitMatrix method in Main.java

--- Main.java
+++ Main.java
@@ -5,8 +5,27 @@ class Main{
         int size = 5;
         int[][] matrix = new int[size][size];
         fillMatrix(matrix);
+        int[][] unitMatrix = getUnitMatrix(5);
         printMatrix(matrix);
+        System.out.println();
+        printMatrix(unitMatrix);
     }
+
+    public static int[][] getUnitMatrix(int size){
+        int[][] unitMatrix = new int[size][size];
+        for (int i = 0; i < unitMatrix.length; i++) {
+            for (int j = 0; j < unitMatrix[i].length; j++) {
+                if (i == j){
+                    unitMatrix[i][j] = 1;
+                }
+                else {
+                    unitMatrix[i][j] = 0;
+                }
+            }
+        }
+        return unitMatrix;
+    }
+
     public static void fillMatrix(int[][] matrix){
         Random random = new Random();
         for (int i = 0; i < matrix.length; i++) {
