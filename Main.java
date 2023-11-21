87fe4d630f6ab457d4cb63de116266a51e5583b2
Timofey <timafolf2003@mail.ru>
2023-11-21 22:09:38 +0300

    add printMatrix method in Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         int[][] matrix = fillRandomMatrix(5, 5);
+        printMatrix(matrix);
     }
     public static int[][] fillRandomMatrix(int rows, int cols) {
         Random random = new Random();
@@ -13,5 +14,15 @@ public class Main {
             }
         }
         return matrix;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        System.out.println("Матрица:");
+        for (int[] row : matrix) {
+            for (int element : row) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
     }
 }
